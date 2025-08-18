cde5de73be6e9c0dd62168f6fa5653350728abf6
Shree Hari P <shreehariparthiban159@gmail.com>
2025-08-18 11:03:15 +0530

    Shopping List - Array

---
+++ shoppingList.java
@@ -0,0 +1,9 @@
+public class shoppingList {
+    public static void main(String[] args){
+        String[] list = {"Butter","Milk","Egg","Bread","Cake"};
+        System.out.println("Shopping List:");
+        for(int i = 0; i < list.length;i++){
+            System.out.println(list[i]);
+        }
+    }   
+}
