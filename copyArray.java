f048fdb51228466f6d84d0b870d27870679f9384
Shree Hari P <shreehariparthiban159@gmail.com>
2025-08-19 12:43:37 +0530

    Copy

---
+++ copyArray.java
@@ -0,0 +1,15 @@
+public class copyArray {
+    public static void main(String[] args){
+        int[] sourceArray = {1, 2, 3, 4, 5};
+        int[] destinationArray = new int[sourceArray.length];
+
+        for(int i = 0; i < sourceArray.length; i++){
+            destinationArray[i] = sourceArray[i];
+        }
+
+        System.out.println("Copied Array: ");
+        for(int num : destinationArray){
+            System.out.print(num+" ");
+        }
+    }
+}
